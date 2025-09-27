31f53657463dbb77d60039bc3ab081e7a50618c2
Otavio <estudosprogotavio@gmail.com>
2025-09-27 10:07:20 -0300

    refactor: Main.py para ficar mais limpa

--- Main.py
+++ Main.py
@@ -1,24 +1,9 @@
-from Server import APIServer
-import threading
+from Controller import Controller
 
-def start_telemetry():
+if __name__ == "__main__":
+    controller = Controller()
     try:
-        from . import mouse_telemetry_server
-        t = threading.Thread(
-            target=mouse_telemetry_server.run_telemetry,
-            kwargs={"host": "0.0.0.0", "port": 6000},
-            daemon=True
-        )
-        t.start()
-        print("[INFO] Telemetry server iniciado em http://0.0.0.0:6000")
-    except Exception as e:
-        print("[WARN] Telemetry server não iniciado:", e)
-
-def main():
-    start_telemetry()
-    server = APIServer()
-    server.register_mouse_endpoints()
-    server.run()
-
-if __name__ == '__main__':
-    main()
+        controller.start()
+    except KeyboardInterrupt:
+        print("Encerrando aplicação...")
+        controller.stop()
