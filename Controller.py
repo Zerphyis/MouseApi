268a57e6145076c925010bd7f416194d6d276c72
Otavio <estudosprogotavio@gmail.com>
2025-09-27 10:22:11 -0300

    refactor:adicionando tratamento de Exception em Controller.py

--- Controller.py
+++ Controller.py
@@ -1,7 +1,11 @@
+from flask import request
 from actions.MouseAction import MouseAction
 from actions.KeyBoardAction import KeyBoardAction
 from actions.MediaAction import MediaAction
 from actions.FileManager import FileManager
+from HandleController import HandleController
+
+handle_controller = HandleController()
 
 class Controller:
     def __init__(self):
@@ -10,23 +14,27 @@ class Controller:
         self.media = MediaAction()
         self.file_manager = FileManager()
 
-    def move_mouse(self, request):
+    @handle_controller.handle
+    def move_mouse(self):
         data = request.get_json()
         x, y = data.get("x"), data.get("y")
         self.mouse.move(x, y)
         return {"status": "ok"}
 
-    def keyboard_type(self, request):
+    @handle_controller.handle
+    def keyboard_type(self):
         data = request.get_json()
         text = data.get("text")
         self.keyboard.type(text)
         return {"status": "ok"}
 
-    def media_play(self, request):
+    @handle_controller.handle
+    def media_play(self):
         self.media.play_pause()
         return {"status": "ok"}
 
-    def file_open(self, request):
+    @handle_controller.handle
+    def file_open(self):
         data = request.get_json()
         path = data.get("path")
         self.file_manager.open(path)
