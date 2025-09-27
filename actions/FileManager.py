e9b6a7f581236f8d1e6377e6bc875c4df508fb79
Otavio <estudosprogotavio@gmail.com>
2025-09-27 10:16:04 -0300

    refactor: FileManager.py para usar IAction

--- actions/FileManager.py
+++ actions/FileManager.py
@@ -1,24 +1,12 @@
 import os
-import time
+from interfaces.IAction import IAction
 
+class FileManager(IAction):
+    def open(self, path):
+        if os.path.exists(path):
+            os.startfile(path)
 
-class FileManager:
-    def __init__(self, upload_dir):
-        self.upload_dir = upload_dir
-        os.makedirs(self.upload_dir, exist_ok=True)
-
-    def save_upload(self, filename, file_bytes):
-        safe_name = os.path.basename(filename)
-        path = os.path.join(self.upload_dir, f"{int(time.time())}_{safe_name}")
-        with open(path, 'wb') as f:
-            f.write(file_bytes)
-        return path
-
-    def list_files(self):
-        return os.listdir(self.upload_dir)
-
-    def get_path(self, name):
-        candidate = os.path.join(self.upload_dir, name)
-        if not os.path.exists(candidate):
-            raise FileNotFoundError(name)
-        return candidate
+    def execute(self, *args, **kwargs):
+        path = kwargs.get("path")
+        if path:
+            self.open(path)
