c3fc9e60aa02fb39a5c71e07c1f0f6ea3dda4d7d
Otavio <estudosprogotavio@gmail.com>
2025-09-29 02:27:52 -0300

    delete: deletando Classe Config.py

--- Config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config:
-    HOST = '0.0.0.0'
-    PORT = 5000
-    API_TOKEN = os.environ.get('PHONE_MOUSE_API_TOKEN', 'troco-esse-token-por-um-seguro')
-    UPLOAD_DIR = os.environ.get('PHONE_MOUSE_UPLOAD_DIR', '/tmp/phone_mouse_uploads')
-    MOUSE_SENSITIVITY = float(os.environ.get('PHONE_MOUSE_SENSITIVITY', '1.0'))
