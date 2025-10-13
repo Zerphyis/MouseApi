d806371c1d5bc39b165ac4cefefbd70087a485cb
Otavio <estudosprogotavio@gmail.com>
2025-10-13 05:58:41 -0300

    Adicionando Config.h

---
+++ esp32_firmware/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define BTN_LEFT   2      
+#define BTN_RIGHT  3      
+#define BTN_SCROLL 4     
+#define JOY_X      0      
+#define JOY_Y      1      
+
+const char* WIFI_SSID = "MouseESP32C3";
+const char* WIFI_PASS = "12345678";
+
+#define MOUSE_SPEED 5     
+#define JOY_DEADZONE 50   
+
+#endif
