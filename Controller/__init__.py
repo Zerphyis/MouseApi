87e3bfcb7bdbb9255e93227db1a2a083b7b45d6b
Otavio <estudosprogotavio@gmail.com>
2025-10-15 08:49:25 -0300

    feat:adicionando __init__.py em pacote Controller

---
+++ Controller/__init__.py
@@ -0,0 +1,13 @@
+from .Controller import Controller
+from .HandleController import HandleController
+from .MouseAction import MouseAction
+from .IAction import IAction
+from .MouseAnalyzer import MouseAnalyzer
+
+__all__ = [
+    "Controller",
+    "HandleController",
+    "MouseAction",
+    "IAction",
+    "MouseAnalyzer"
+]
