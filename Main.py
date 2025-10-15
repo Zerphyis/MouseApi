f288e10d21fcc6079d87b3ffdd15c1aea3010078
Otavio <estudosprogotavio@gmail.com>
2025-10-15 08:47:45 -0300

    refactor:Corrigindo imports em Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,9 @@
 from App import app
-from ArduinoController import ArduinoController
+from Hardware import ArduinoController
 
 def main():
     arduino = ArduinoController(port="COM3", baudrate=9600)
     arduino.start_listening()
-
     app.run(host="0.0.0.0", port=5000)
 
 if __name__ == "__main__":
